b7d63aa6ef13e0161bd36d352fa86d27e7977acb
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2024-04-26 22:37:02 -0300

    tentativa

--- banco/src/Main.java
+++ banco/src/Main.java
@@ -2,38 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
+    
     public static void main(String[] args) {
-
-        String nome;
-        int conta = 1;
-        double saldo = 500;
-
         Scanner dados = new Scanner(System.in);
 
-        System.out.println("insira o que deseja fazer: \n 1- para criar conta \n 2- para fazer deposito \n 3- para " +
-                "sacar");
+        System.out.println("escolha uma opção: \n 1 - para saque \n 2 - para deposito \n 3- para saldo");
         int opcao = dados.nextInt();
 
-        if (opcao == 1) {
-            dados.nextLine();
-            System.out.println("insira seu nome: ");
-            nome = dados.nextLine();
-            conta = conta + 1;
-            System.out.println(conta);
-        } else if (opcao == 2) {
-            System.out.println("insira o valor do deposito: ");
-            double deposito = dados.nextDouble();
-            double saldoT = saldo + deposito;
-            System.out.println(saldoT);
-            saldo = saldoT;
-        } else if (opcao == 3) {
-            System.out.println("insira o valor para sacar: ");
-            double saque = dados.nextDouble();
-            double vSaque = saldo - saque;
-            System.out.println(vSaque);
-            saldo = vSaque;
-        }
+        double saldo = 500;
 
-        System.out.printf("sua conta é %d, seu saldo é %.2f", conta,saldo);
+        switch (opcao) {
+            case 1:
+            System.out.println("insira o valor do saque: ");
+            Double saque = dados.nextDouble();
+            Double novoSaldo = saldo - saque;
+            saldo=novoSaldo;
+            System.out.println("o valor sacado foi: "+ saque);
+            System.out.println("seu saldo é: " + novoSaldo);
+                break;
+
+            case 2:
+                System.out.println("insira o valor de deposito: ");
+                double deposito = dados.nextDouble();
+                novoSaldo = saldo + deposito;
+                saldo=novoSaldo;
+                System.out.println("o valor depositado foi: " + deposito);
+                System.out.println("seu saldo é: " + novoSaldo);
+                break;
+        
+                case 3:
+                System.out.println("seu saldo é: " + saldo);
+                break;
+            default:
+                break;
+        }
     }
 }
